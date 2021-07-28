9f91a0d012cf20214532881be7254fd07359b4ea
Aaksha Sharma <60940606+Aaksha-sharma@users.noreply.github.com>
2021-07-28 13:12:28 +0530

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,12 @@
+void operations(int a, int b, int c){
+    
+    // Your code here
+    long long int P=pow(a,b);
+    long long int XOR=P^c;
+    long long int rem=P%c;
+    cout<<P<<endl;
+    cout<<XOR<<endl;
+    cout<<rem<<endl;
+    
+    
+}
